fc37f5aa31ba23f4956dac19d1c587c3b1217c49
dalao-B <zhibin61@163.com>
2019-04-16 17:42:30 +0800

    创建站点

--- demo01/booktest/admin.py
+++ demo01/booktest/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from django.contrib import admin
+from .models import BookInfo,HeroInfo
+# Register your models here.
+admin.site.register(BookInfo)
+admin.site.register(HeroInfo)
--- demo01/booktest/models.py
+++ demo01/booktest/models.py
@@ -6,9 +6,15 @@ class BookInfo(models.Model):
     btitle = models.CharField(max_length=20)
     bpub_date = models.DateTimeField()
 
+    def __str__(self):
+        return "%d %s" % (self.pk, self.btitle)
+
+
 
 class HeroInfo(models.Model):
     hname = models.CharField(max_length=20)
     hgender = models.BooleanField()
     hcontent = models.CharField(max_length=50)
     hbook = models.ForeignKey('BookInfo', on_delete=models.CASCADE)
+    def __str__(self):
+        return "%d %s" % (self.pk, self.hname)
