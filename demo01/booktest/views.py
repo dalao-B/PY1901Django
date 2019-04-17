173c4b58cd8c2e202c9c2879dbcbb24b97c9571c
dalao-B <zhibin61@163.com>
2019-04-17 16:54:18 +0800

    创建使用模板

--- demo01/booktest/views.py
+++ demo01/booktest/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.template import loader, RequestContext
+from .models import BookInfo, HeroInfo
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    # template = loader.get_template('booktest/index.html')
+    context = {"username":"aaa"}
+    return render(request, 'booktest/index.html', context)
+    # result = template.render(context=context)
+    # return HttpResponse(result)
+    # return HttpResponse("index")
+
+
+def detail(request, id):
+    # template = loader.get_template('booktest/detail.html')
+    book = BookInfo.objects.get(pk=id)
+    context = {"book": book}
+    return render(request, 'booktest/detail.html', context)
+    # result = template.render(context=context)
+    # return HttpResponse("详情页 %s" %id)
+    # return HttpResponse("detail %s" %id)
+
+
+def list(request):
+    # template = loader.get_template('booktest/detail.html')
+    booklist = BookInfo.objects.all()
+    context = {"booklist":booklist}
+    return render(request, 'booktest/list.html', context)
+    # result = template.render(context=context)
+    # return HttpResponse(result)
+    # return HttpResponse("liebiaoye")
