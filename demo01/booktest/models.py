ba479b90811fab849e60a764230922dcda0e861a
dalao-B <zhibin61@163.com>
2019-04-17 11:15:11 +0800

    自动子管理页面实现关联注册

--- demo01/booktest/models.py
+++ demo01/booktest/models.py
@@ -9,6 +9,15 @@ class BookInfo(models.Model):
     def __str__(self):
         return "%d %s" % (self.pk, self.btitle)
 
+    def name(self):
+        return self.btitle
+    name.short_description = "书名"
+
+
+    def date(self):
+        return self.bpub_date
+    date.short_description = "日期"
+
 
 
 class HeroInfo(models.Model):
@@ -18,3 +27,19 @@ class HeroInfo(models.Model):
     hbook = models.ForeignKey('BookInfo', on_delete=models.CASCADE)
     def __str__(self):
         return "%d %s" % (self.pk, self.hname)
+
+    def skill(self):
+        return self.hcontent
+    skill.short_description = "英雄技能"
+
+
+    def sxingbie(self):
+        return self.hgender
+    sxingbie.short_description = "性别"
+
+
+    def sname(self):
+        return self.hname
+    sname.short_description = "姓名"
+
+
